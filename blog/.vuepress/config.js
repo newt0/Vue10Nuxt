47253aba6bb69b8f5e22c5930d17644826c2eb18
newt0 <py_lambda@yahoo.co.jp>
2020-06-01 14:18:39 +0900

    fix: サイト雛形を登録

---
+++ blog/.vuepress/config.js
@@ -0,0 +1,211 @@
+module.exports = {
+  title: 'VuePressクイックスターター for Blog',
+  description: 'Vuepressでブログを始めるためのクイックスターターです。最短最速でサイト構築を完了させることで、記事を書くことに専念できます。',
+  theme: require.resolve('../../'),
+  themeConfig: {
+    /**
+     * Ref: https://vuepress-theme-blog.ulivz.com/config/#dateformat
+     */
+
+    // dateFormat: 'YYYY-MM-DD',
+
+    /**
+     * Ref: https://vuepress-theme-blog.ulivz.com/config/#nav
+     */
+
+    nav: [
+      {
+        text: 'Blog',
+        link: '/',
+      },
+      {
+        text: 'Tags',
+        link: '/tag/',
+      },
+      {
+        text: '本サイト',
+        link: 'https://vue10nuxt.netlify.app/',
+      },
+      // {
+      //   text: 'Location',
+      //   link: '/location/',
+      // },
+    ],
+
+    /**
+     * Ref: https://vuepress-theme-blog.ulivz.com/config/#footer
+     */
+    footer: {
+      contact: [
+        // {
+        //   type: 'codepen',
+        //   link: '/',
+        // },
+        // {
+        //   type: 'codesandbox',
+        //   link: '/',
+        // },
+        // {
+        //   type: 'facebook',
+        //   link: '/',
+        // },
+        {
+          type: 'github',
+          link: 'https://github.com/newt0',
+        },
+        // {
+        //   type: 'gitlab',
+        //   link: '/',
+        // },
+        // {
+        //   type: 'instagram',
+        //   link: '/',
+        // },
+        // {
+        //   type: 'linkedin',
+        //   link: '/',
+        // },
+        // {
+        //   type: 'mail',
+        //   link: '/',
+        // },
+        // {
+        //   type: 'messenger',
+        //   link: '/',
+        // },
+        // {
+        //   type:'music',
+        //   link:'/'
+        // },
+        // {
+        //   type: 'phone',
+        //   link: '/',
+        // },
+        {
+          type: 'twitter',
+          link: 'https://twitter.com/vue_nuxt',
+        },
+        // {
+        //   type:'video',
+        //   link:'/'
+        // },
+        {
+          type: 'web',
+          link: 'https://qiita.com/kyohei_ai',
+        },
+        // {
+        //   type: 'youtube',
+        //   link: '/'
+        // }
+      ],
+      copyright: [
+        // {
+        //   text: 'Privacy Policy',
+        //   link: 'https://policies.google.com/privacy?hl=en-US',
+        // },
+        {
+          text: 'MIT Licensed | Copyright © 2018-present Vue.js',
+          link: '',
+        },
+      ],
+    },
+    /**
+     * Ref: https://vuepress-theme-blog.ulivz.com/config/#directories
+     */
+
+    // directories:[
+    //   {
+    //     id: 'post',
+    //     dirname: '_posts',
+    //     path: '/',
+    //     itemPermalink: '/:year/:month/:day/:slug',
+    //   },
+    //   {
+    //     id: 'writing',
+    //     dirname: '_writings',
+    //     path: '/',
+    //     itemPermalink: '/:year/:month/:day/:slug',
+    //   },
+    // ],
+
+    /**
+     * Ref: https://vuepress-theme-blog.ulivz.com/config/#frontmatters
+     */
+
+    // frontmatters: [
+    //   {
+    //     id: "tag",
+    //     keys: ['tag', 'tags'],
+    //     path: '/tag/',
+    //   },
+    //   {
+    //     id: "location",
+    //     keys: ['location'],
+    //     path: '/location/',
+    //   },
+    // ],
+
+    /**
+     * Ref: https://vuepress-theme-blog.ulivz.com/config/#globalpagination
+     */
+
+    globalPagination: {
+      lengthPerPage: 10,
+    },
+
+    /**
+     * Ref: https://vuepress-theme-blog.ulivz.com/config/#sitemap
+     */
+    sitemap: {
+      hostname: 'https://example.vuepress-theme-blog.ulivz.com/'
+    },
+    /**
+     * Ref: https://vuepress-theme-blog.ulivz.com/config/#comment
+     */
+    comment: {
+      service: 'disqus',
+      shortname: 'vuepress-plugin-blog',
+    },
+    /**
+     * Ref: https://vuepress-theme-blog.ulivz.com/config/#newsletter
+     */
+    newsletter: {
+      endpoint: 'https://billyyyyy3320.us4.list-manage.com/subscribe/post?u=4905113ee00d8210c2004e038&amp;id=bd18d40138'
+    },
+    /**
+     * Ref: https://vuepress-theme-blog.ulivz.com/config/#feed
+     */
+    feed: {
+      canonical_base: 'https://example.vuepress-theme-blog.ulivz.com/',
+    },
+
+    /**
+     * Ref: https://vuepress-theme-blog.ulivz.com/config/#summary
+     */
+
+    // summary:false,
+
+    /**
+     * Ref: https://vuepress-theme-blog.ulivz.com/config/#summarylength
+     */
+
+    summaryLength:100,
+
+    /**
+     * Ref: https://vuepress-theme-blog.ulivz.com/config/#pwa
+     */
+
+    pwa:true,
+
+    /**
+     * Ref: https://vuepress-theme-blog.ulivz.com/config/#paginationcomponent
+     */
+
+    // paginationComponent: 'SimplePagination'
+
+    /**
+     * Ref: https://vuepress-theme-blog.ulivz.com/config/#smoothscroll
+     */
+    smoothScroll: true
+  },
+}
